d86eb43604300918a9115613b70b435d85f76e2d
Jean Karlo <jeankarlodev@gmail.com>
2026-07-18 07:51:02 -0300

    init project

---
+++ src/main/java/com/vantrack/VantrackApplication.java
@@ -0,0 +1,13 @@
+package com.vantrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VantrackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VantrackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vantrack/VantrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vantrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VantrackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
