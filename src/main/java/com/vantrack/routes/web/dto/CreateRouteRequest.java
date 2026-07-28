7db6295e6b7617bcc54c1658ff67e684a2f10ab0
Jean Karlo <jeankarlodev@gmail.com>
2026-07-28 20:55:17 -0300

    adds validation in create route request

--- src/main/java/com/vantrack/routes/web/dto/CreateRouteRequest.java
+++ src/main/java/com/vantrack/routes/web/dto/CreateRouteRequest.java
@@ -1,12 +1,19 @@
 package com.vantrack.routes.web.dto;
 
 import com.vantrack.routes.Route;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.util.UUID;
 
 
 public record CreateRouteRequest (
+        @Size(max = 200, message = "O nome deve conter no máximo 200 caracteres")
+        @NotEmpty(message = "O nome é obrigatório e não pode estar vazio")
         String name,
+
+        @NotNull(message = "O id da van é obrigatório")
         UUID vanId
 ) {
     public Route toDomain() {
